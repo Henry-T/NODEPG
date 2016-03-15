a525945f84d1d9c2e890ca0c2f01012b3a598d0d
Henry.T <tang.hy@live.com>
2016-03-15 13:29:54 +0800

    temp

--- ToolSite/app.js
+++ ToolSite/app.js
@@ -74,6 +74,54 @@ app.post('/android/logparse/upload', function(req, res) {
     });
 });
 
+app.get('/httpclient/hysg_build', function(req, res){
+  res.sendfile("hysg_build.html");
+})
+
+app.post('/httpclient/hysg_build/request', function(req, res) {
+    res.charset = 'utf-8'
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var branch = fields.branch;
+        var channels = fields.channels;
+        var channelAry = channels.split(",");
+
+        for(var i=0; i<channelAry.length; i++) {
+            var channel = channelAry[i];
+
+            var path = "/job/Build_Android/buildWithParameters?GAME=hysg&SVN_BRANCH_DIR="+branch+"&SVN_REVISION=HEAD&DEPENDENCY_CONFIG=GSDKWorkflow='product';QuickRevision='HEAD';GSDKRelease=false;CocosCodebaseRevision='HEAD';AnySDK=false;&PLATFORM_CONFIG=Platform='"+channel+"';&HARDWARE_CONFIG=ControlFramework='key';EnableTouch=false;&I18N_CONFIG=Loc='zh_CN';FakeLoc=false;&PACKAGE_CONFIG=CompileMode='release';EnableUpdate=true;EnableMainUpgrade=true;&DEBUG_CONFIG=ShowFPS=false;EnableLog=false;PayByPlatform=true;PayTest=false;&GAME_CONFIG=AnalyticsReport=true;ShowGuide=true;USE_FINGER_TOUCH=true;SKIP_INTRO=false;";
+            console.log(path);
+
+            var options = {
+                hostname: 'jenkins.apowo.com',
+                port: 80,
+                path: path,
+                auth: 'test:test',
+                method: 'POST',
+                header: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': 0
+                }
+            };
+
+            var successCount = 0;
+
+            var reqIn = http.request(options, (resIn)=>{
+                if (resIn.statusCode == 200)
+                    successCount += 1;
+                console.log("statusCode:"+resIn.statusCode);
+            });
+            reqIn.end();
+            console.log("end one");
+        }
+        console.log("out of loop");
+        // res.write("success: "+successCount + "/" + channelAry.length);
+        res.write("request count: " + channelAry.length);
+        res.end();
+    });
+});
+
+
 app.listen(10001, function () {
   console.log('Example app listening on port 10001!');
 });
